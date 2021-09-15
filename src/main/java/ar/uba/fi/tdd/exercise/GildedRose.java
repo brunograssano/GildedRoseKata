7407454e496162294098ff5d19b9cf6b54d79abe
brunograssano <grassanobruno@gmail.com>
2021-09-15 14:40:02 -0300

    ref: Changed for to foreach

--- src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
+++ src/main/java/ar/uba/fi/tdd/exercise/GildedRose.java
@@ -11,52 +11,52 @@ class GildedRose {
     // update the quality of the emements
     public void updateQuality() {
         // for each item
-        for (int i = 0; i < items.length; i++) {
+        for (Item item : items) {
 
-            if (!items[i].Name.equals("Aged Brie")
-                    && !items[i].Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0)
-                    if (!items[i].Name.equals("Sulfuras, Hand of Ragnaros"))
-                        items[i].quality = items[i].quality- 1;
+            if (!item.Name.equals("Aged Brie")
+                    && !item.Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0)
+                    if (!item.Name.equals("Sulfuras, Hand of Ragnaros"))
+                        item.quality = item.quality- 1;
             } else { // Aged Brie case
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                           // si es menor que 6
-                        if (items[i].sellIn < 6) {
+                        if (item.sellIn < 6) {
                             // si la cantidad es es menor que 50
-                            if (items[i].quality < 50) {
+                            if (item.quality < 50) {
                                 // sumo 1 a la calidad
-                                items[i].quality = items[i].quality + 1;
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
             }}
 
-            if (!items[i].Name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.Name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].Name.equals("Aged Brie")) {
-                    if (!items[i].Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].Name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.Name.equals("Aged Brie")) {
+                    if (!item.Name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.Name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else {  // Aged Brie case
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
           }
             }
